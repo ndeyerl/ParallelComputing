9d3f5b295df12749b96513174d9bbfe9f96cb219
ndeyerl <ndeyerl@smu.edu>
2017-04-05 20:18:18 -0500

    lab 9 cpp commit'

--- Lab9/quadrature.cpp
+++ Lab9/quadrature.cpp
@@ -3,17 +3,17 @@
    Math 4370/6370
    18 March 2015 */
 
-
-/* Inclusions */
-#include <stdlib.h>   /* malloc(), free() */
-#include <stdio.h>    /* printf() */
+// Inclusions 
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
 #include <math.h>
 #include "mpi.h"
 
-/* macros */
+// macros
 #define PI 3.14159265358979323846
 
-/* Prototypes */
+// Prototypes 
 inline double f(double a, double b) { return (a*exp(3.0*a)*sin(25.0*PI*b)); }
 
 
@@ -25,8 +25,8 @@ inline double f(double a, double b) { return (a*exp(3.0*a)*sin(25.0*PI*b)); }
    over sub-squares of fixed size 1/n x 1/n. */ 
 int main(int argc, char* argv[]) {
 
-  /* declarations */
-  int n = 1000;
+  // declarations
+  int n=1000;
   int i, j, k, l, is, ie, js, je, ierr, numprocs, myid;
   double F, F_all, h, x, y, a, b, stime, ftime, runtime;
   double F_true = 2.0/225.0/PI*(2.0*exp(3.0)+1.0);
@@ -51,28 +51,28 @@ int main(int argc, char* argv[]) {
   // intialize MPI
   ierr = MPI_Init(&argc, &argv);
   if (ierr != MPI_SUCCESS) {
-    fprintf(stderr," error in MPI_Init = %i\n",ierr);
+    std::cerr << " error in MPI_Init = " << ierr << "\n";
     return 1;
   }
   ierr = MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   if (ierr != MPI_SUCCESS) {
-    fprintf(stderr," error in MPI_Comm_size = %i\n",ierr);
+    std::cerr << " error in MPI_Comm_size = " << ierr << "\n";
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
   ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myid);
   if (ierr != MPI_SUCCESS) {
-    fprintf(stderr," error in MPI_Comm_rank = %i\n",ierr);
+    std::cerr << " error in MPI_Comm_rank = " << ierr << "\n";
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
   // root outputs parallelism information to screen
   if (myid == 0)
-    printf(" Running with %i MPI tasks\n",numprocs);
+    std::cout << " Running with " << numprocs << " MPI tasks\n";
 
-  /* start timer */
+  // start timer 
   stime = MPI_Wtime();
 
-  /* set subinterval width */
+  // set subinterval width 
   h = 1.0 / n;
 
   // determine this processor's sub-region of the 2D region
@@ -84,51 +84,54 @@ int main(int argc, char* argv[]) {
   je = ((int) (1.0*n/numprocs))*(myid+1);
   if (myid == numprocs-1)  je = n;
 
-  /* initialize local result */
+  // initialize local result
   F = 0.0;
 
-  /* perform integration over n intervals in each direction */
+  // perform integration over n intervals in each direction
   for (i=is; i<ie; i++) {
     for (j=js; j<je; j++) {
 
-      /* in each sub-square, evaluate at all 64 points and combine results */
+      // in each sub-square, evaluate at all 64 points and combine results
       for (k=0; k<nodes; k++) {
         for (l=0; l<nodes; l++) {
 
-          /* location of sub-square center */
+          // location of sub-square center
           x = h * (i + 0.5);
           y = h * (j + 0.5);
 	  
-          /* location of quadrature evaluation point */
+          // location of quadrature evaluation point
           a = x + 0.5*h*z[k];
           b = y + 0.5*h*z[l];
 
-          /* update numerical integral with contribution from point */
+          // update numerical integral with contribution from point
           F += 0.25*h*h*w[k]*w[l]*f(a,b);
 
-        }  /* end l loop */
-      }  /* end k loop */
-    }  /* end j loop */
-  }  /* end i loop */
+        }  // end l loop
+      }  // end k loop
+    }  // end j loop
+  }  // end i loop
 
   // perform reduction to get result on root process
   ierr = MPI_Reduce(&F, &F_all, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
   if (ierr != MPI_SUCCESS) {
-    fprintf(stderr," error in MPI_Reduce = %i\n",ierr);
+    std::cerr << " error in MPI_Reduce = " << ierr << "\n";
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
-  /* stop timer */
+  // stop timer 
   ftime = MPI_Wtime();
   runtime = ftime - stime;
 
-  /* root outputs computed value and error */
+  // root outputs computed value and error
   if (myid == 0) {
-    printf(" computed F = %.16e\n",F_all);
-    printf("     true F = %.16e\n",F_true);
-    printf("      error = %.4e\n",fabs(F_true-F_all));
-    printf("    runtime = %g\n",runtime);
+    std::cout << " computed F = " << std::setprecision(16) << F_all << "\n";
+    std::cout << "     true F = " << F_true << "\n";
+    std::cout << "      error = " << std::setprecision(5) << fabs(F_true-F_all) << "\n";
+    std::cout << "    runtime = " << runtime << "\n";
   }
 
-} /* end main */
+  // finalize MPI
+  ierr = MPI_Finalize();
+
+} // end main 
 
